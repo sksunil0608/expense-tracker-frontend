eb14a6c808b9a71e875074b60280ee3acfe4ed4b
sksunil0608 <sksunil0608@gmail.com>
2024-02-07 02:09:32 +0530

    Setup of Exprense Tracker App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <div>Title:</div>
+        <div>Ammount:</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
