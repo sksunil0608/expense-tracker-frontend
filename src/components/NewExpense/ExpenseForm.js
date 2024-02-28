adecaba480e6d8c41b4fa30d659b7ee245aafefd
sksunil0608 <sksunil0608@gmail.com>
2024-02-28 23:37:22 +0530

    multiple ways to update form input in state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,18 +2,29 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseFrom = () => {
 
-  const [enteredTitle,setEnteredTitle] = useState('')
-  const [enteredAmount,setEnteredAmount] = useState('')
-  const [enteredDate,setEnteredDate] = useState('')
+  const [userInput,setUserInput] = useState({
+    enteredTitle:'',
+    enteredAmount:'',
+    enteredDate:''
+    })
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value)
+    // setUserInput({...userInput,enteredTitle:event.target.value})
+    setUserInput((prevState)=>{
+      return {...prevState,enteredTitle:event.target.value}
+    })
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value)
+    // setUserInput({...userInput,enteredAmount:event.target.value})
+    setUserInput((prevState)=>{
+      return {...prevState,enteredAmount:event.target.value}
+    })
   };
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value)
+    // setUserInput({...userInput,enteredDate:event.target.value})
+    setUserInput((prevState)=>{
+      return {...prevState,enteredDate:event.target.value}
+    })
   };
   return (
     <div className="container">
