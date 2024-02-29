68ad2f80fb8f6bd62c2fba8da709122a7185e0d3
sksunil0608 <sksunil0608@gmail.com>
2024-02-29 15:56:50 +0530

    Added date filter in the expense

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expense.css";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 
-export default function Expense({expenses}) {
-  console.log(expenses)
+export default function Expense({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   const LocationofExpenditure = "Noida";
   return (
     <Card className="expenses">
-      {expenses.length!==0 && expenses.map((expense, i) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          date={expense.date}
-          amount={expense.amount}
-          location={LocationofExpenditure}
+      {
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-      ))}
+      }
+      {expenses.length !== 0 &&
+        expenses
+          .filter((expense) => {
+            const year = new Date(expense.date).getFullYear().toString();
+            return year === filteredYear;
+          })
+          .map((expense, i) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              date={expense.date}
+              amount={expense.amount}
+              location={LocationofExpenditure}
+            />
+          ))}
     </Card>
   );
 }
